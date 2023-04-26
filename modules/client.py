d6bc82a73930753d920ccd96120b3720149327b3
dominik228820 <dominik228820@gmail.com>
2023-04-26 17:52:56 +0200

    Klient

---
+++ modules/client.py
@@ -0,0 +1,7 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox, Label
+from tkinter_uix.Entry import Entry
+import mysql.connector as sql
+import modules.login as l
+from modules.creds import user_pwd
