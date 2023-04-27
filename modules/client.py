b2f6420149b7af3c94ca2e48dd38634ea113b793
dominik228820 <dominik228820@gmail.com>
2023-04-27 16:56:40 +0200

    Pobranie danych klienta

--- modules/client.py
+++ modules/client.py
@@ -5,3 +5,19 @@ from tkinter_uix.Entry import Entry
 import mysql.connector as sql
 import modules.login as l
 from modules.creds import user_pwd
+
+def pobierz_dane(email):
+    global name, location, gen, clicid
+    q = f'select CName,CLocation,CGender,CID from mydb.client where CEmail="{email}"'
+    mycon = sql.connect(host='localhost', user='root',
+	passwd=user_pwd, database='mydb')
+
+
+    cur = mycon.cursor()
+    cur.execute(q)
+    d = cur.fetchall()
+    mycon.close()
+    name = d[0][0]
+    location = d[0][1]
+    gen = d[0][2]
+    clicid = d[0][3]
