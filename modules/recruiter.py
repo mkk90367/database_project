0238a434f622db73316341232587f7e8fe5c2519
mkk90367 <119660944+mkk90367@users.noreply.github.com>
2023-04-27 09:17:20 +0200

    Update recruiter.py
    dodano funkcję uzyskajInformacje, która daje możliwość wyekstrahowania informacji na temat konkretnej osoby rekrutującej z tabeli rekruter przy pomocy adresu email;
    zastosowano polecenie sql SELECT...WHERE

--- modules/recruiter.py
+++ modules/recruiter.py
@@ -5,3 +5,18 @@ from tkinter_uix.Entry import Entry
 import mysql.connector as sql
 import modules.login as l
 from modules.creds import user_pwd
+
+def uzyskajInformacje(email):
+    global name, company, gen, recid
+    q = f'select RName,CompanyName,RGender,RID from mydb.recruiter where REmail="{email}"'
+    mycon = sql.connect(host='localhost', user='root',
+                        passwd=user_pwd, database='mydb')
+    cur = mycon.cursor()
+    cur.execute(q)
+    d = cur.fetchall()
+    mycon.close()
+
+    name = d[0][0]
+    company = d[0][1]
+    gen = d[0][2]
+    recid = d[0][3]
