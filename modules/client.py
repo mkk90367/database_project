761d707bcb0cc0d8ea81305127ed664020203ae3
dominik228820 <dominik228820@gmail.com>
2023-04-29 12:55:16 +0200

    Aplikuj i usun

--- modules/client.py
+++ modules/client.py
@@ -30,9 +30,41 @@ def logi(root):
     l.log(root)
 	
 def aplikuj(table):
-  selectedindex = table.focus()
-  
-  mycon = sql.connect(host='localhost', user='root',
+   
+    selectedindex = table.focus()    
+    selectedvalues = table.item(selectedindex, 'values')
+    ajid = selectedvalues[0]
+    chkquery = f'SELECT * from mydb.application where cid={clicid} and jid={ajid}'
+    mycon = sql.connect(host='localhost', user='root',
                         passwd=user_pwd, database='mydb')
-  cur = mycon.cursor()
-  mycon.close()
+    cur = mycon.cursor()
+    cur.execute(chkquery)
+    tempbuff = cur.fetchall()
+    mycon.close()
+    if(tempbuff):
+        messagebox.showinfo('Uwaga', 'Wygląda na to, że już złożyłeś wniosek o tę pracę')
+    else:
+        queryapplyjob = f'Insert into application values(NULL,(select rid from mydb.job where job.jid={ajid}),{ajid},{clicid})'
+        mycon = sql.connect(host='localhost', user='root',
+                            passwd=user_pwd, database='mydb')
+        cur = mycon.cursor()
+        cur.execute(queryapplyjob)
+        mycon.commit()
+        mycon.close()
+        messagebox.showinfo('Twój wniosek został złożony')
+	
+		
+def usun(table):
+    selectedindex = table.focus()
+    selectedvalues = table.item(selectedindex, 'values')
+    aaid = selectedvalues[0]
+    mycon = sql.connect(host='localhost', user='root',
+                        passwd=user_pwd, database='mydb')
+    cur = mycon.cursor()
+    cur.execute(
+        f'delete from mydb.application where aid={aaid}')
+    mycon.commit()
+    mycon.close()
+    messagebox.showinfo('Twój wniosek został złożony')
+    myapp()
+
