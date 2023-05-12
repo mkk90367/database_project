f6c6869305947cc5b1c48d502bf9d227ab65ad85
dominik228820 <dominik228820@gmail.com>
2023-05-12 17:21:45 +0200

    prawa str

--- modules/client.py
+++ modules/client.py
@@ -288,3 +288,16 @@ def client(root, email1):
         'normal', 20), bg="#b32e2e", fg="#ffffff", command=moje_aplikacje)
     ap.grid(row=1, column=0, padx=60, pady=70)
 	
+	#Prawo
+	
+	
+	global rt, tab, bgr
+    rt = Frame(root, width=540, height=420, bg="#ffffff")
+    rt.place(x=450, y=220)
+    tab = Frame(root, bg="#FFFFFF")
+    tab.place(x=460, y=300, width=520, height=350)
+    bgrf = Frame(root, width=540, height=420)
+    bgrf.load = PhotoImage(file="elements\\bgr.png")
+    bgr = Label(root, image=bgrf.load, bg="#00b9ed")
+    bgr.place(x=440, y=210)
+	
